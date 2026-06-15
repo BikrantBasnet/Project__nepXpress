f2d027a2bc7db4b10105088198cc1ae29b0ba0f6
BikrantBasnet <bikrantbasnet25@gmail.com>
2026-06-15 14:49:32 +0545

    common db made with railway

--- app/models/database.py
+++ app/models/database.py
@@ -17,12 +17,12 @@ class Database:
     def __init__(self):
         self.connection = pymysql.connect(
             host=config.MYSQL_HOST,
+            port=config.MYSQL_PORT,
             user=config.MYSQL_USER,
             password=config.MYSQL_PASSWORD,
             database=config.MYSQL_DATABASE,
             charset='utf8mb4'
         )
-
     def fetch_one(self, query, params=None):
         cursor = self.connection.cursor(pymysql.cursors.DictCursor)
         cursor.execute(query, params)
